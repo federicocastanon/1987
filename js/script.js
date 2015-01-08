fcb74eb18e543dd1a03932a0fb7f1a5d6fd12068
kstoriadis <federico.gonzalezc@gmail.com>
2015-01-08 20:43:19 -0300

    Nueva galería
    Lista toda la funcionalidad del portfolio, falta agregar las imágenes
    definitivas y hacer los textos para cada parte

--- js/script.js
+++ js/script.js
@@ -10,51 +10,74 @@ $(document).ready(function() {
 			slidesToShow : 2,
 			slidesToScroll : 1
 		});
-		$(".proyecto > div").each(function(index) {
-			$(this).height($(this).width() / 1.81);
+		var mayorAltura = 200;
+		$(".proyecto").each(function(index) {
+			$(this).find('div').height($(this).find('div').width() / 1.81);
+			var alturaActual = $(this).height();
+			if (alturaActual > mayorAltura) {
+				mayorAltura = alturaActual;
+			}
 		});
 
+		$('#wrapperDetalleProyectos').height($(window).height() * 0.9);
+		$('#wrapperDetalleProyectos').width($(window).width() * 0.8);
+
+		$(document).keyup(function(e) {
+			if (e.keyCode == 27) {
+				$("#lean_overlay").trigger("click");
+			}
+		});
 		$('.masDetalle').each(function(index) {
 			$(this).leanModal({
 				top : 40,
 				overlay : 0.4,
 				closeButton : ".modal_close"
 			});
+
 			$(this).click(function() {
 				var indice = parseInt($(this).attr("indice"));
 				if (inicializado) {
-						$('.sliderDetalle').slickGoTo(indice);
-						$('.sliderFotos').slickGoTo(indice);
+					$('.sliderDetalle').slickGoTo(indice);
+					$('.sliderFotos').unslick();
+					var nuevaGaleria = $('#galeria' + indice).html();
+					$('.sliderFotos').html(nuevaGaleria);
+					$('.sliderFotos').slick({
+						slidesToShow : 1,
+						slidesToScroll : 1,
+						dots : false,
+						centerMode : true,
+						focusOnSelect : true,
+						infinite : true
+					});
 				} else {
 					$('.sliderDetalle').slick({
 						slidesToShow : 1,
 						slidesToScroll : 1,
 						arrows : false,
 						fade : true,
-						asNavFor : '.sliderFotos',
-						infinite : true,
 						initialSlide : indice
 					});
+					var nuevaGaleria = $('#galeria' + indice).html();
+					$('.sliderFotos').html(nuevaGaleria);
 					$('.sliderFotos').slick({
 						slidesToShow : 1,
 						slidesToScroll : 1,
-						asNavFor : '.sliderDetalle',
 						dots : false,
 						centerMode : true,
 						focusOnSelect : true,
-						infinite : true,
-						initialSlide : indice
+						infinite : true
 					});
 					inicializado = true;
 				}
 			});
 		});
-		$('#wrapperDetalleProyectos').height($(window).height() * 0.9);
-		$('#wrapperDetalleProyectos').width($(window).width() * 0.8);
-
+		$(".slick-list").height(mayorAltura * 1.1);
+		//$(".slick-track").height(mayorAltura * 1.1);
+		$("#cuerpo").height(mayorAltura * 1.3);
+	} else {
+		var altura = $(".contenidoPrincipal").outerHeight();
+		//el cuerpo crece con el contenido, al wrapper hay que hacerlo crecer
+		$("#cuerpo").height(altura);
 	}
 
-	var altura = $(".contenidoPrincipal").height() * 1.2;
-	//el cuerpo crece con el contenido, al wrapper hay que hacerlo crecer
-	$("#cuerpo").height(altura);
 });
